2be1ee8ca00c41867cafdb32e312deb2e15f8950
aphelps231 <aphelps231@outlook.com>
2017-01-12 14:49:38 -0800

    x9

--- CSCD/Track Time/src/trackTimes/Track.java
+++ CSCD/Track Time/src/trackTimes/Track.java
@@ -1,23 +1,58 @@
 package trackTimes;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Track
 {
-	static double mps = 1.0, fps = 3.28084, kph = 3.6, mph = 2.23694;
+	static double mps = 1.0, fps = 3.28084, kph = 3.6, mph = 2.23694, yps = 1.09361;
+
+	static String[] messages = new String[]
+	{ " meters per second,", " feet per second,", " kilometers per hour,", " miles per hour," };
+
+	static Scanner s;
 
 	public static void main(String[] args)
 	{
-		System.out.println(Arrays.toString(runConversions(5.0)));
-
-		System.out.println("suh dudes");
-		
-		System.out.println("test");
+		runProgram();
 	}
 
-	public static double[] runConversions(double time)
+	public static void runProgram()
 	{
-		double metersPerSecond = time / 100.0;
+		s = new Scanner(System.in);
+		double input;
+		double[] times;
+
+		System.out.print("Please enter the winning time of the race: ");
+
+		input = Double.parseDouble(s.nextLine());
+
+		times = runVelocityConversions(input);
+		
+		for(int i = 0; i < times.length; i++)
+		{
+			System.out.println(times[i] + messages[i]);
+		}
+		
+		int minutes;
+		double seconds;
+		
+		times = runTimeConversions(input);
+		
+		times[0] *= 60.0; //seconds
+		minutes = (int) times[0];
+		seconds = round(times[0] - minutes);
+		
+		System.out.println("It would take " + minutes + " minutes and " + seconds + " seconds for the person to run one mile.");
+		
+		times[1] *= 100.0;
+		seconds = round(times[1]);
+		
+		System.out.println("It would take " + seconds + " seconds for the person to run one hundred yards.");
+	}
+
+	public static double[] runVelocityConversions(double time)
+	{
+		double metersPerSecond = 100.0 / time;
 
 		double[] times = new double[]
 		{ mps * metersPerSecond, fps * metersPerSecond, kph * metersPerSecond, mph * metersPerSecond };
@@ -29,12 +64,25 @@ public class Track
 
 		return times;
 	}
+	
+	public static double[] runTimeConversions(double time)
+	{
+		double	metersPerSecond = 100.0 / time,
+				mphN = mph * metersPerSecond,
+				ypsN = yps * metersPerSecond,
+				hpmN = 1.0 / mphN,
+				spyN = 1.0 / ypsN;
+		
+		return new double[] { hpmN, spyN };
+	}
 
 	public static double round(double num)
 	{
-		double a = num % 0.01;
-		double x = num - a;
+		int a = (int) (num * 100.0);
+		double x = (double) (a / 100.0);
 
-		return a >= .0049 ? x + .01 : x;
+		//System.out.println(num + " : " + a + " : " + x);
+		
+		return num - x >= .005 ? x + .01 : x;
 	}
 }
